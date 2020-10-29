8d0b353030b020fafb0ec25c0d8fa246f3d71a4e
Emil <emil.valbak.hermansen@gmail.com>
2020-10-29 14:11:12 +0100

    added java test code

---
+++ src/test/java/TestTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class TestTest {
+    @Test
+    public void quickTest() {
+
+        // assert statements
+        assertEquals(0, 0);
+    }
+}
